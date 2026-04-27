5c173517dc577f65639cf5b5cda1f086b2ea9a41
leslieannumali031788 <umalileslieann10@gmail.com>
2026-04-27 23:50:02 +0800

    Added print statement.

--- file_handling_gwa/highest_gwa.py
+++ file_handling_gwa/highest_gwa.py
@@ -9,4 +9,5 @@ class HighestGwa:
     def get_highest(self, data: list):
         new_data = [info.split(",") for info in data]
         highest_gwa = max(new_data, key=lambda x: x[1])
+        print(f"Highest Gwa: {highest_gwa[0]}\nGwa: {highest_gwa[1]}")
         
