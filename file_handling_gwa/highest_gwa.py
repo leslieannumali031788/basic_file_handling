93de780a0bb9d861b994ac15bc9cd1745c3fc58d
leslieannumali031788 <umalileslieann10@gmail.com>
2026-04-27 23:47:05 +0800

    Reads the file.

--- file_handling_gwa/highest_gwa.py
+++ file_handling_gwa/highest_gwa.py
@@ -3,7 +3,8 @@ class HighestGwa:
         self.filename = filename
 
     def read_file(self) -> list[str]:
-        pass
+        with open(self.filename, "r") as file:
+            return [line.rstrip("\n") for line in file.readlines()]
 
     def get_highest(self, data: list):
         pass
