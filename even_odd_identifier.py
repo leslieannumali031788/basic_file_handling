c1d515cdb4dcee2c2b3981d5cb486d450293bd67
leslieannumali031788 <umalileslieann10@gmail.com>
2026-04-27 21:26:19 +0800

    Removed the close paranthesis on the second line, mitigating the error committed.

--- even_odd_identifier.py
+++ even_odd_identifier.py
@@ -1,5 +1,5 @@
 class EvenOddIdentifier:
-    def __init__(self, filename): str = "./numbers.txt"): # sets up the first state
+    def __init__(self, filename: str = "./numbers.txt"): # sets up the first state
         pass
 
     def read_file(self) -> list[int]:
