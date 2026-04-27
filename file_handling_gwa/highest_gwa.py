fe01d08820523a576c145651396a8a879c81db5d
leslieannumali031788 <umalileslieann10@gmail.com>
2026-04-27 23:44:08 +0800

    Defined class.

---
+++ file_handling_gwa/highest_gwa.py
@@ -0,0 +1,9 @@
+class HighestGwa:
+    def __init__(self, filename: str = "gwa.txt"):
+        pass
+
+    def read_file(self) -> list[str]:
+        pass
+
+    def get_highest(self, data: list):
+        pass
