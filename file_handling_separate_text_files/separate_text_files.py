bfe4e2382a0e83bec812da775a8182a24dabc76e
leslieannumali031788 <umalileslieann10@gmail.com>
2026-04-28 12:12:18 +0800

    First encapsulation.

---
+++ file_handling_separate_text_files/separate_text_files.py
@@ -0,0 +1,12 @@
+class DataDistributor:
+    def __init__(self, filename: str = "integers.txt"):
+        self.filename = filename
+
+    def read_file(self) -> list[str]:
+        pass
+
+    def write_file(self, filename: str, content: int):
+        pass
+
+    def process_file(self):
+        pass
