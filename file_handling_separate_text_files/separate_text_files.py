d4c66d4f5b26a5b00713553409f713e87fd06ed3
leslieannumali031788 <umalileslieann10@gmail.com>
2026-04-28 12:15:27 +0800

    Erased -> list[str], changed to self.

--- file_handling_separate_text_files/separate_text_files.py
+++ file_handling_separate_text_files/separate_text_files.py
@@ -4,8 +4,8 @@ class DataDistributor:
         self.even_file = "double.txt"
         self.odd_file = "triple.txt"
 
-    def read_file(self) -> list[str]:
-        pass
+    def read_file(self):
+
 
     def write_file(self, filename: str, content: int):
         pass
